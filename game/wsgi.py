0486bdb5c08fe705eea61e75f1d58a6fd6b38fbe
ferous-bura <alemayehubiruh@gmail.com>
2025-02-02 03:08:47 +0300

    Update wsgi.py
    for render only

--- game/wsgi.py
+++ game/wsgi.py
@@ -7,13 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
-import sys
+#import os
+#import sys
 
 # Path to your project
-path = '/home/lotterybingo/bingo'
-if path not in sys.path:
-    sys.path.append(path)
+#path = '/home/lotterybingo/bingo'
+#if path not in sys.path:
+#    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
