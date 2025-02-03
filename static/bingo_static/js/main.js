98bf68da254afde781c805f98851b4862e612bab
ferous-bura <alemayehubiruh@gmail.com>
2025-02-03 23:03:07 +0300

    Update main.js
    start time lower

--- static/bingo_static/js/main.js
+++ static/bingo_static/js/main.js
@@ -796,7 +796,7 @@ $(document).ready(function () {
                 playSpecialAudio("readyPlay.mp3");
 
                 // Start the game after a short delay to allow the start audio to finish
-                setTimeout(() => playGame(), 3000);
+                setTimeout(() => playGame(), 500);
             },
             function (errorMessage) {
                 resetGameConfirmed();
